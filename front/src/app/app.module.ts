17b35ea0f7eae9d8f2bbb162b315493be3399590
morphelin <arifleu@gmail.com>
2017-09-14 14:39:18 +0200

    mise a jour composants et architecture

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './authentication/authentication/authentication.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 import { ProductComponent } from './products/product/product.component';
 import { LandingComponent } from './products/landing/landing.component';
-import { CartComponent } from './cart/cart/cart.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { ProfileComponent } from './user/profile/profile.component';
 
