629936fe8746504b0b594212fdb54e9d779c9112
tangorri <tangorri@live.fr>
2017-09-15 12:39:55 +0200

    Authenfication component et Page Store

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { ProfileComponent } from './user/profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/store/store.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  styleUrls: ['store.component.css'],
+  selector: 'app-store',
+  templateUrl: 'store.component.html'
+})
+
+export class NameComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
